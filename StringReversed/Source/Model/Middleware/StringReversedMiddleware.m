7771e5403d4e39c1513665527439a5e4bd062404
Nghia Tran <vinhnghiatran@gmail.com>
2017-08-04 15:01:01 +0700

    Implement Process method in StringReversedMiddleware

--- StringReversed/Source/Model/Middleware/StringReversedMiddleware.m
+++ StringReversed/Source/Model/Middleware/StringReversedMiddleware.m
@@ -11,6 +11,39 @@
 @implementation StringReversedMiddleware
 
 -(nonnull NSString *) process:(nonnull NSString *) text {
+
+    // Convert to array
+    NSMutableArray<NSString *> *stringComponents = [self toArray:text];
+
+    // Prepare data
+    NSUInteger length = stringComponents.count;
+    NSUInteger halfPoint = length / 2;
+
+    // Swap first character to last character
+    // And goes back to the halft point
+    // It's save haft of time
+    // Althought time complexity = O(n). But actually it's n/2
+    // with n is lengh of text
+
+    for (NSUInteger i = 0; i < halfPoint; i++) {
+        NSString *temp = stringComponents[i];
+        NSUInteger indicator = length - 1 - i;
+
+        // Swap
+        stringComponents[i] = [stringComponents[indicator] copy];
+        stringComponents[indicator] = [temp copy];
+    }
+
+    // Build
+    NSString *result = [self buildStringFromArray:stringComponents];
+    return result;
+}
+
+-(NSMutableArray<NSString *> *) toArray:(NSString *) text {
+    return @[];
+}
+
+-(NSString *) buildStringFromArray:(NSArray<NSString *> *) stringComponents {
     return @"";
 }
 
